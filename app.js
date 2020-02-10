74fa7b13557b03cac5ea60c8615ce6e0884a1747
Mr. Rohit <singh.rohitrajput94@gmail.com>
2020-02-10 19:02:10 +0530

    create customer

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ var app = express();
 app.listen(3000, function () {
     console.log("server running");
 });
+
+//create new customer
+var createCustomer = function () {
+    var param ={};
+    param.email ="mike@gmail.com";
+    param.name="Mike";
+    param.description ="from node";
+
+    stripe.customers.create(param, function (err,customer) {
+        if(err)
+        {
+            console.log("err: "+err);
+        }if(customer)
+        {
+            console.log("success: "+customer)
+        }else{
+            console.log("Something wrong")
+        }
+    })
+
+}
+
+createCustomer();
